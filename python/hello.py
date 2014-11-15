c5bb30d1f07429f7f8d9b0c7ffc55be97db26e06
xc145214 <xc145214@gmail.com>
2014-11-16 00:34:22 +0800

    练习的python文件

---
+++ python/hello.py
@@ -0,0 +1,83 @@
+#hello.py
+#!/usr/bin/env python
+
+
+# output
+print 'Hello world'
+
+print 'THe quick brown fox','jumps over','the lazy dog'
+
+print 300
+
+print 100 + 200
+
+print '100 + 200 =',100+200
+
+name = raw_input('please enter your name:')
+
+print 'hello, ' +name
+
+#print absolute value of an integer;
+a = 100
+if a>=0:
+	print a
+else:
+	print -a
+
+#data type
+print 'I\'m \"OK\"!'
+
+print 'I\'m learning \nPython.'
+
+print '\\\n\\'
+
+print '\\\t\\'
+
+print  r'\\\t\\'
+
+print ''' line1
+...line2
+...line3'''
+
+True
+
+False
+
+3 > 2
+
+3 > 5
+
+True or True
+
+True or False
+
+False or False
+
+not True
+
+not False
+
+if age >= 18:
+	print 'adult'
+else:
+	print 'teenager'
+
+#value
+
+a = 1
+
+t_007 = 'T007'
+
+Answer = True
+
+a = 123 # a is an int number
+print a
+a = 'ABC' # a is a String
+print a 
+
+int a  = 123
+a = 'ABC'
+
+x = 10 
+x = x + 2
+
